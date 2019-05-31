7134d2a2372457416735f0998f3d1d56cc0646be
Luciano Gambarte <lucho_ign@hotmail.com>
2019-05-31 10:39:53 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/content/search/search.component';
+import { CompletionComponent } from './components/content/completion/completion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SearchComponent,
+    CompletionComponent
   ],
   imports: [
     BrowserModule,
